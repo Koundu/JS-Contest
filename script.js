85792919c7ac3578fd77eaff4ea6ed6a1e7b7e9b
Koundu <121346044+Koundu@users.noreply.github.com>
2023-06-24 23:51:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var fullname = document.getElementById("Full-Name");
+var email = document.getElementById("email");
+var pass = document.getElementById("Password");
+var fnameout = document.getElementById("fname");
+var emailout = document.getElementById("email");
+var passout = document.getElementById("pass")
+
+btn-signUp.addEventListener("click", function(){
+    var userData = {
+        "fullname": fullname.value,
+        "email": email.value,
+        "lastname":lastname.value
+    }
+})
+
